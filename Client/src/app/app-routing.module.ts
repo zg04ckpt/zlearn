4b408f8263f057df5802fdb7c24b8a200829f8ee
zg04ckpt <nguyenhc424@gmail.com>
2024-04-29 21:58:04 +0700

    complete show list question set UI

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListQuestionSetComponent } from './components/list-question-set/list-question-set.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListQuestionSetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListQuestionSetComponent } from './components/list-question-set/list-question-set.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListQuestionSetComponent
   ],
   imports: [
     BrowserModule,
