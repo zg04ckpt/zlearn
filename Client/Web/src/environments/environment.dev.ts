b70167fe3718367b88517f3d304332cd5a124ba8
zg04ckpt <nguyenhc424@gmail.com>
2024-12-06 21:30:27 +0700

    fix docker volume

--- Client/Web/src/environments/environment.dev.ts
+++ Client/Web/src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: false,
     baseUrl: 'http://localhost:5000',
     defaultImageUrl: 'http://localhost:5000/api/images/test/default.jpg',
     defaultAvtUrl: 'http://localhost:5000/api/images/user/default.jpg'
